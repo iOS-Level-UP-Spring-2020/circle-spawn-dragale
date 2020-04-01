3e841bb5ab6524237be67802b737c1b5c4a284dc
radosslaw <radosslaw@github.com>
2020-04-01 15:00:04 +0200

    Update CircleSpawnController.swift

--- CircleSpawn/CircleSpawn/CircleSpawnController.swift
+++ CircleSpawn/CircleSpawn/CircleSpawnController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class CircleSpawnController: UIViewController {
+class CircleSpawnController: UIViewController, UIGestureRecognizerDelegate {
 
 	// TODO: Assignment 1
 
@@ -12,9 +12,14 @@ class CircleSpawnController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let tap = UITapGestureRecognizer(target: self, action: #selector(spawnCircle(_:)))
-        tap.numberOfTapsRequired = 2
-        view.addGestureRecognizer(tap)
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(spawnCircle(_:)))
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        view.addGestureRecognizer(doubleTap)
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 
     @objc func spawnCircle(_ tap: UITapGestureRecognizer) {
@@ -34,6 +39,47 @@ class CircleSpawnController: UIViewController {
             spawnedView.transform = .identity
         })
         
+        let tripleTap = UITapGestureRecognizer(target: self, action: #selector(destroyCircle(_:)))
+        tripleTap.numberOfTapsRequired = 3
+        spawnedView.addGestureRecognizer(tripleTap)
+        
+        let grab = UILongPressGestureRecognizer(target: self, action: #selector(moveCircle(_ :)))
+        spawnedView.addGestureRecognizer(grab)
+    }
+    
+    @objc func destroyCircle(_ tap: UITapGestureRecognizer) {
+        let spawnedView = tap.view!
+        
+        UIView.animate(withDuration: 0.2, animations: {
+            spawnedView.alpha = 0
+            spawnedView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+        }, completion: {completed in
+            spawnedView.removeFromSuperview()
+        })
+    }
+    
+    @objc func moveCircle(_ longTap: UILongPressGestureRecognizer) {
+        guard let circle = longTap.view else {return}
+        let point = longTap.location(in: self.view)
+        
+        if longTap.state == UIGestureRecognizer.State.began {
+            UIView.animate(withDuration: 0.2, animations: {
+                circle.alpha = 0.6
+                circle.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            })
+        }
+        
+        if longTap.state == UIGestureRecognizer.State.changed {
+            circle.center.x = point.x
+            circle.center.y = point.y
+        }
+        
+        if longTap.state == UIGestureRecognizer.State.ended {
+            UIView.animate(withDuration: 0.2, animations: {
+                circle.alpha = 1
+                circle.transform = .identity
+            })
+        }
     }
 }
 
