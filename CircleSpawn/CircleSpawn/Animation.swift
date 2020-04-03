ac4acc1412e55722dc1ee566ec47a7be85a6c5aa
Dominik Drag <dominikdrag98@gmail.com>
2020-04-03 19:42:27 +0200

    Create Animation.swift
    For handling animations

---
+++ CircleSpawn/CircleSpawn/Animation.swift
@@ -0,0 +1,43 @@
+//
+//  Animation.swift
+//  CircleSpawn
+//
+//  Created by Dominik Drąg on 03/04/2020.
+//  Copyright © 2020 DaftAcademy. All rights reserved.
+//
+
+import UIKit
+
+class Animation {
+    func spawn(object: UIView) {
+        object.alpha = 0
+        object.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        UIView.animate(withDuration: 0.2, animations: {
+            object.alpha = 1
+            object.transform = .identity
+        })
+    }
+    
+    func despawnAnimation(object: UIView) {
+        UIView.animate(withDuration: 0.2, animations: {
+            object.alpha = 0
+            object.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        }, completion: { completed in
+            object.removeFromSuperview()
+        })
+    }
+    
+    func pickUp(object: UIView) {
+        UIView.animate(withDuration: 0.2, animations: {
+            object.alpha = 0.5
+            object.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        })
+    }
+    
+    func putDown(object: UIView) {
+        UIView.animate(withDuration: 0.2, animations: {
+            object.alpha = 1
+            object.transform = .identity
+        })
+    }
+}
